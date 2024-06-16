dbe55dde6e1a2b12aaa898495787dd63c06f45e5
WillliamsL <w.lauvergne@gmail.com>
2024-06-16 19:08:17 +0200

    maj 16062024

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+const container = document.querySelector("body");
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    if(name && email && message) {
-        alert(`Merci pour votre message, ${name}!`);
-        // Ici, vous pourriez ajouter une fonctionnalité pour envoyer les données à un serveur.
-    } else {
-        alert('Veuillez remplir tous les champs.');
-    }
+container.addEventListener("mousemove", (e) => {
+  const rect = container.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  container.style.setProperty("--x", `${x}px`);
+  container.style.setProperty("--y", `${y}px`);
 });
